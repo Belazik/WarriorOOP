09373b0f7c47c200dad3729aafaf5b9c2f399cdb
Kotletta-TT <assimilation13cd@gmail.com>
2020-01-27 23:19:33 +0300

    First Commit

---
+++ app.py
@@ -0,0 +1,6 @@
+import random
+from wariorlib import Warrior, Fight
+
+
+count_warriors = [i for i in range(1,random.randint(2,5))]
+current_warrior = []
---
+++ wariorlib.py
@@ -0,0 +1,42 @@
+import random
+
+
+random
+
+
+class Warrior:
+    def setName(self, name):
+        self.name = name
+
+    def setRace(self, race):
+        self.race = race
+        if self.race == 'nigger':
+            health = 250
+        elif self.race == 'vegan':
+            health = 220
+        else:
+            health = 200
+        return self.health
+
+    def setWeapon(self, weapon):
+        self.weapon = weapon
+        if self.weapon == 'sword':
+            strength = 35
+        elif self.weapon == 'knife':
+            strength = 25
+        else:
+            strength = 20
+        return strength
+
+
+
+class Fight:
+    def start(self, **kwargs):
+
+
+
+
+
+weapon = ['sword', 'knife', 'hands']
+race = ['niger', 'vegan', 'hipster']
+name = ['BillyG', 'Thunder', 'Morgenshtern', 'Yarik']
